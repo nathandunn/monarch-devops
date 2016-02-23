1ff63a32bbc8aa4d480f12d9fdbff5ec960abbeb
Jeremy Nguyen Xuan <jnguyenxuan@lbl.gov>
2016-02-22 17:10:58 -0800

    added letsencrypt playbook

---
+++ ansible/ohsu-migration/roles/letsencrypt/files/renew.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+service nginx stop  # or whatever your webserver is
+if ! /opt/letsencrypt/letsencrypt-auto renew -nvv --standalone > /var/log/letsencrypt/renew.log 2>&1 ; then
+    echo Automated renewal failed:
+    cat /var/log/letsencrypt/renew.log
+    exit 1
+fi
